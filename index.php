704712179bedddc2d04a8c63563027923187b9e4
Andres Martinez <andres.e.martinez@outlook.com>
2017-02-12 03:22:43 +0000

    Update changes to new project

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+header('HTTP/1.1 307 Temporary Redirect');
+header('Location: yii2/backend/web');
+?>
+<html>
+  <head>
+    <title>Moved</title>
+  </head>
+  <body>
+    <h1>Moved</h1>
+    <p>This page has moved to <a href="yii2/backend/web">YII</a>.</p>
+  </body>
+</html>
